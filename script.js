75de2592a8ff1d7550f96306557db74857969cef
lapiskun <296770098+lapiskun@users.noreply.github.com>
2026-07-14 13:41:46 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-Document.querySelectorAll('.butterfly ').forEach(function (el) {
-  el.addEventListener('mouseover', function () {
-    el.style.left = (Math.random() * 800) + "px";
-  });
+document.querySelectorAll('.butterfly').forEach(function (el) {
+el. addEventListener('mouseover', function () {
+el. style. left = (Math. random() * 800) + "px";
+});
 });
 
 document.querySelectorAll('.foo').forEach(function (el) {
-  el.addEventListener('mouseover', function () {
-    el.style.left = (Math.random() * 300 + 400) + "px";
-    el.style.top = (Math.random() * 80 + 200) + "px";
-  });
+el. addEventListener('mouseover', function () {
+el. style. left = (Math. random() * 300 + 400) + "px";
+el. style. top = (Math. random() * 80 + 200) + "px";
+});
 });
